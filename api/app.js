0046ce34ca742170431ee94eb5e7f4401b6e2b88
Carlos La Fuente <carlos.lafuente.dev@gmail.com>
2022-01-07 12:30:46 -0400

    cors config

--- api/app.js
+++ api/app.js
@@ -5,18 +5,8 @@ const cors = require('cors');
 const routes = require('./app/routes/index.route');
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', routes);
-app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
-  );
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
 
 module.exports = app;
