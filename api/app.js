c7247d1d756fcd782540d190a4a1376ec19cf496
Carlos La Fuente <carlos.lafuente.dev@gmail.com>
2022-01-07 12:24:13 -0400

    header configuration added

--- api/app.js
+++ api/app.js
@@ -8,5 +8,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/api', routes);
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 module.exports = app;
