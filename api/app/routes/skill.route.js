aff46d39567b335879fe24573dbdce1fa8e0cb44
Carlos La Fuente <carlos.lafuente.dev@gmail.com>
2022-01-07 12:21:02 -0400

    Skills middleware removed

--- api/app/routes/skill.route.js
+++ api/app/routes/skill.route.js
@@ -2,7 +2,7 @@ const { apiGET, apiPOST } = require('../controllers/skill.controller');
 const { isLogged } = require('../middlewares/auth.middleware');
 
 const skillRoute = (router) => {
-  router.get('/skills', isLogged, apiGET);
+  router.get('/skills', apiGET);
   router.post('/skills', isLogged, apiPOST);
 };
 
