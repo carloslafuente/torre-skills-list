4172856c2960901961410e5d25070eb56573b00d
Carlos La Fuente <carlos.lafuente.dev@gmail.com>
2022-01-07 12:19:32 -0400

    Cors policy added

--- api/app.js
+++ api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const routes = require('./app/routes/index.route');
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/api', routes);
+app.use(cors());
 
 module.exports = app;
